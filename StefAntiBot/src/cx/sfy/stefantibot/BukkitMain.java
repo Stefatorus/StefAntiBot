12ca212bf266976580c68ec9538fa923d07bcde9
Beelzebu <jsuarez.lara@outlook.com>
2018-08-20 08:20:13 -0300

    Improve async tasks and use try with resources for autocloseable

--- StefAntiBot/src/cx/sfy/stefantibot/BukkitMain.java
+++ StefAntiBot/src/cx/sfy/stefantibot/BukkitMain.java
@@ -3,41 +3,24 @@ package cx.sfy.stefantibot;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitMain extends JavaPlugin implements Listener {
 
-	public static Plugin p;
-	
 	@Override
 	public void onEnable() {
-		p = this;
-		getServer().getPluginManager().registerEvents(this, this);
+		Bukkit.getPluginManager().registerEvents(this, this);
 	}
-	
-	@Override
-	public void onDisable() {
-		
-	}
-	
+
 	@EventHandler
-	public void onJoin(PlayerJoinEvent e) {
-		String ip = e.getPlayer().getAddress().getHostName();
-		Bukkit.getScheduler().runTaskAsynchronously(p, new Runnable() {
-			@Override
-			public void run() {
-				
-				if (BlacklistMGR.isProxy(ip)) {
-					System.out.println(ip);
-					BlacklistMGR.blacklistIP(ip);
-				}
-				
+	public void onJoin(AsyncPlayerPreLoginEvent e) {
+		String ip = e.getAddress().getHostName();
+		Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+			if (BlacklistMGR.isProxy(ip)) {
+				System.out.println(ip);
+				BlacklistMGR.blacklistIP(ip);
 			}
 		});
 	}
-	
-	
-	
 }
