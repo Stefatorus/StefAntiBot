8fb6583dd3ef8efb81507162f3df50e1c01d104a
Stefan D <stefatorus@mcfast.net>
2019-03-03 17:13:35 +0200

    Improved firewall considerably

--- StefAntiBot/Firewall Tools/Firewall.sh
+++ StefAntiBot/Firewall Tools/Firewall.sh
@@ -1,18 +1,17 @@
 ipset=/sbin/ipset
 iptables=/sbin/iptables
 route=/etc/acl
-zone=/etc/zones
+zone=zones
+
+wgetd="wget -q -c --retry-connrefused -t 0"
+
+if [ ! -d $zone ]; then mkdir -p $zone; fi
+		echo "Downloading country ip ranges..."
+        $wgetd http://www.ipdeny.com/ipblocks/data/countries/all-zones.tar.gz && tar -C $zone -zxvf all-zones.tar.gz >/dev/null 2>&1 && rm -f all-zones.tar.gz >/dev/null 2>&1
+
 
 # What port should we protect against bots?
-port=666
-
-# This are the settings for the firewall. You can change this 
-# to account to your playerbase. This is a must!
-limit=300
-
-# Limit max persec conns.
-seconds=5
-maxper=5
+port=25565
 
 # Limit new connections per second. This is highly useful when
 # somebody starts an attack because it basically makes his attack
@@ -21,11 +20,9 @@ maxper=5
 # Don't rely solely on this as this doesn't really fix the issue,
 # just get rid of suspicious IP's.
 burstconns=40
-normalconns=15
 
-# These settings work along with the plugin to block proxies
+# TODO: Plugin whitelist
 timeout=900
-
 
 # This works by having whitelisted countries that will allways
 # be able to connect to your server. Meanwhile, the people from
@@ -38,10 +35,13 @@ timeout=900
 
 # Create Whitelist IPSET
 
+
+echo "Preparing ipset configuration"
 $ipset -F
 $ipset -N -! proxies hash:net maxelem 150000 timeout $timeout
 $ipset -N -! whitelist hash:net maxelem 1000000
- for ip in $(cat $zone/whitelist.zone); do
+ # This is where you select which countries you want whitelisted. It's really important.
+ for ip in $(cat $zone/{ro,md,fr,de,ie,gb,it,at,tr,bg,es}.zone); do
   $ipset -A whitelist $ip
  done
  
@@ -52,13 +52,12 @@ $iptables -N BotManager
 $iptables -F BotManager
 
 # Step 2. Check for the max connection amount to mitigate the attack.
-$iptables -A BotManager -p tcp -m set --match-set proxies src -j DROP
-$iptables -A BotManager -p tcp -m set --match-set whitelist src -j ACCEPT
-$iptables -A BotManager -p tcp --syn --dport $port -m recent --update --seconds $seconds --hitcount $maxper -j DROP
-$iptables -A BotManager -p tcp --syn --dport $port -m connlimit --connlimit-above $limit --connlimit-mask 0 -j DROP
-$iptables -A BotManager -m state --state RELATED,ESTABLISHED -m limit --limit $normalconns/second --limit-burst $burstconns -j ACCEPT
-$iptables -A BotManager -p tcp --dport $port -j DROP
+$iptables -A BotManager -p tcp --dport $port -m set --match-set proxies src -j DROP
+$iptables -A BotManager -p tcp --dport $port -m set --match-set whitelist src -j ACCEPT
+$iptables -A BotManager -p tcp --dport $port --syn -m limit --limit $burstconns/s -j ACCEPT
+$iptables -A BotManager -p tcp --dport $port --syn -j DROP
 
 # Add BotManager to iptables and remove it just in case it is already there.
 $iptables -D INPUT -p tcp -j BotManager
 $iptables -A INPUT -p tcp -j BotManager
+echo "Firewall applied successfully."
